d06777a6ab1302ab81b0a7a5798c7b0f0d884057
mazusu <qweghj1245@gmail.com>
2021-03-28 16:56:23 +0800

    feat(post): comment place

--- pages/post/SectionContent.tsx
+++ pages/post/SectionContent.tsx
@@ -1,3 +1,4 @@
+import { Row } from "@styles/flexStyle";
 import fontStyle from "@styles/fontStyle";
 import { Comment, PostTagsMap, RelationPost } from "api/PostApi";
 import React from "react";
@@ -38,6 +39,42 @@ const NoPost = styled.p`
   ${fontStyle("12px", "17px", "bold")};
 `;
 
+const WriteCommentWrapper = styled.div`
+  padding: 0 16px;
+  background: ${({ theme: { color } }) => color.grey100};
+`;
+
+const WriteComment = styled(Row)`
+  padding: 14px 0;
+  border-radius: 20px;
+  box-shadow: 0px 3px 12px #e0e0e0;
+  background: ${({ theme: { color } }) => color.white};
+`;
+
+const MemberImage = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  margin-right: 8px;
+`;
+
+const LinearGradient = styled.div`
+  text-align: center;
+  padding-bottom: 4px;
+  background: linear-gradient(to left, #52b6f9, #4c3cef) left bottom #ffffff
+    no-repeat;
+  background-size: 100% 2px;
+`;
+
+const AskingText = styled.span`
+  ${fontStyle("14px", "20px", "bold")};
+`;
+
+const ResponseText = styled.span`
+  color: ${({ theme: { color } }) => color.primary};
+  ${fontStyle("14px", "20px", "bold")};
+`;
+
 export enum SectionType {
   COMMENT = "comment",
   RELATION = "relation",
@@ -53,12 +90,25 @@ type Props = {
 export default function SectionContent(props: Props) {
   const { sectionType, comments, relationPosts, postTags } = props;
 
+  const WriteCommentComponent = () => (
+    <WriteCommentWrapper>
+      <WriteComment justifyContent="center">
+        <MemberImage src="" alt="" />
+        <LinearGradient>
+          <AskingText>對這篇文章有想法嗎？</AskingText>
+          <ResponseText>留下回應</ResponseText>
+        </LinearGradient>
+      </WriteComment>
+    </WriteCommentWrapper>
+  );
+
   switch (sectionType) {
     case SectionType.COMMENT:
       if (comments?.length === 0)
         return (
           <>
             <Title>文章回應</Title>
+            {WriteCommentComponent()}
             <NoPost>目前沒有回應</NoPost>
           </>
         );
@@ -66,6 +116,7 @@ export default function SectionContent(props: Props) {
       return (
         <>
           <Title>文章回應</Title>
+          {WriteCommentComponent()}
           <Wrapper>
             {comments?.map((comment) => (
               <CommentCard key={comment.id} comment={comment} />
